31c52ad53997fa9803528222ec847b6ae4fe25c1
liamonk <61438758+liamonk@users.noreply.github.com>
2024-06-11 09:31:46 +1200

    navigation bar changed away from sidenav to own react router version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,56 @@
-import { Link, useHistory } from "react-router-dom"
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import logo from "/src/assets/logo.png";
 
-export const Header = () => {
-  const history = useHistory()
+const StyledHeaderMain = styled.div`
+  background-color: #b0e1ff;
+  padding: 10px;
+  margin-bottom: 20px;
+  box-shadow: 5px 5px lightgrey;
+  display: flex;
+  margin-left: auto
+`;
+
+const StyledNavButton = styled.li`
+display: block;
+margin-left: 50px;
+a {
+    text-decoration: none;
+    color: black;
+  }
+background-color: pink;
+border-radius: 5px;
+padding-left: 20px;
+padding-right: 20px;
+`
+
+const StyledNavArea = styled.ul`
+  list-style-type: none;
+  padding-left: 20px;
+  display: flex;
+  justify-content: space-around;
+  margin-left:auto;
+`
+
+const StyledImage = styled.img`
+  height: 70px;
+  margin-left: auto;
+  margin-top: auto;
+  margin-bottom: auto;
+`;
+
+export const Header =()=> {
   return (
-    <>
-     <SideNav
-                onSelect={(selected : string) => {
-                    const to = '/' + selected;
-                    if (location.pathname !== to) {
-                        history.push(to);
-                    }
-                }}
-            >
-                <SideNav.Toggle />
-                <SideNav.Nav defaultSelected="home">
-                    <NavItem eventKey="PiAndSeek">
-                        <NavIcon>
-                            <i className="fa fa-pie-chart" style={{ fontSize: '1.75em' }} />
-                        </NavIcon>
-                        <NavText>
-                            PiAndSeek
-                        </NavText>
-                    </NavItem>
-                    <NavItem eventKey="countdown">
-                        <NavIcon>
-                            <i className="fa fa-hourglass" style={{ fontSize: '1.75em' }} />
-                        </NavIcon>
-                        <NavText>
-                            Countdown
-                        </NavText>
-                    </NavItem>
-                </SideNav.Nav>
-            </SideNav>
-      <h1>RealApps</h1>
-    </>
+    <StyledHeaderMain>
+      <h1>Real Apps</h1>
+      <StyledNavArea>
+        
+          <StyledNavButton><Link to="/countdown"><p>Countdown</p></Link></StyledNavButton>
+          <StyledNavButton><Link to="/piandseek"><p>PiAndSeek</p></Link></StyledNavButton>
+        
+        </StyledNavArea>  
+      <StyledImage src={logo} />      
+    </StyledHeaderMain>
   );
-};
+}
