18cea4f8c800053a479059aef219151949808c61
liamonk <61438758+liamonk@users.noreply.github.com>
2024-09-04 21:37:56 +1200

    changing basename to github variable

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router basename="/RealApps">
+    <Router basename={process.env.BASE_NAME ?? '/'}>
       <App />
     </Router>
   </React.StrictMode>
