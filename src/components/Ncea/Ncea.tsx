542ff84fb34d4b3e0162dd174bf3eaf366bbaeed
liamonk <61438758+liamonk@users.noreply.github.com>
2024-09-12 12:36:25 +1200

    adding to NCEA page with some more useful links

--- src/components/Ncea/Ncea.tsx
+++ src/components/Ncea/Ncea.tsx
@@ -1,13 +1,26 @@
 export const Ncea = ()=>{
     return(
-        <>
+    <>
+    <div style={{display:'flex'}}>
+        <div style ={{padding: '20px', margin: "10px", backgroundColor: 'lightGrey', borderRadius: '2px'}}>
             <h1>Here are a collection of useful NCEA resources</h1>
-            <h2>Level 1 mathematics</h2>
+            <h2>Level 1 mathematics:</h2>
                 <h3><a href="https://www.nzqa.govt.nz/ncea/assessment/search.do?query=math&view=exams&level=01&_gl=1*1z0diwx*_ga*OTg3OTMzMTc0LjE3MjMwMDI5Nzc.*_ga_TFQQ681L2E*MTcyNjA5OTY3OC4zMS4xLjE3MjYwOTk3MzUuMC4wLjA.">External exams</a></h3>
-            <h2>Level 2 mathematics</h2>
+            <h2>Level 2 mathematics:</h2>
                 <h3><a href="https://www.nzqa.govt.nz/ncea/assessment/search.do?query=math&view=exams&level=02">External exams</a></h3>
-            <h2>Level 3 mathematics</h2>
+            <h2>Level 3 mathematics:</h2>
                 <h3><a href="https://www.nzqa.govt.nz/ncea/assessment/search.do?query=math&view=exams&level=03">External exams</a></h3>
-        </>
+        </div>
+        <div style ={{padding: '20px', margin: "10px", backgroundColor: 'lightGrey', borderRadius: '2px'}}>
+            <h2>Useful websites for resources:</h2>
+                <h3><a href="https://sites.google.com/view/infinityplusone/home?authuser=0">Infinity plus one</a>- Fantastic videos going over NCEA exams</h3>
+                <h3><a href="https://www.draustinmaths.com/">Dr Austin maths</a>- Great for printable worksheets for different topics</h3>
+                <h3><a href="https://grapher.jake4maths.com/">NZGrapher</a>- Awesome Graphing website for NCEA internals</h3>
+                <h3><a href="https://nzamtnews.blogspot.com/">NZAMT news</a>-  to help stay up to date with Mathematics Education</h3>
+                <h3><a href="https://nrich.maths.org/">NRich</a>- For rich mathematical activities</h3>
+                <h3><a href="https://tahurangi.education.govt.nz/">Tahurangi</a> - NZ Curriculum information</h3>
+        </div>
+    </div>
+    </>
     )
 }
