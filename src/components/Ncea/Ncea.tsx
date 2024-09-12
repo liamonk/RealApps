b56e5c75aeaeba9bbbf3c9db23b5bc6d242920b0
liamonk <61438758+liamonk@users.noreply.github.com>
2024-09-13 09:26:18 +1200

    some changes to NCEA page (links and descriptions)

--- src/components/Ncea/Ncea.tsx
+++ src/components/Ncea/Ncea.tsx
@@ -1,16 +1,18 @@
 export const Ncea = () => {
   return (
     <>
+      <h1>Here are a collection of useful maths resources</h1>
       <div style={{ display: "flex" }}>
         <div
           style={{
             padding: "20px",
             margin: "10px",
-            backgroundColor: "lightGrey",
+            backgroundColor: "#E8E8E8",
             borderRadius: "2px",
+            width: "30%",
           }}
         >
-          <h1>Here are a collection of useful NCEA resources</h1>
+          <h2>NCEA resources</h2>
           <h2>Level 1 mathematics:</h2>
           <h3>
             <a href="https://www.nzqa.govt.nz/ncea/assessment/search.do?query=math&view=exams&level=01&_gl=1*1z0diwx*_ga*OTg3OTMzMTc0LjE3MjMwMDI5Nzc.*_ga_TFQQ681L2E*MTcyNjA5OTY3OC4zMS4xLjE3MjYwOTk3MzUuMC4wLjA.">
@@ -34,31 +36,32 @@ export const Ncea = () => {
           style={{
             padding: "20px",
             margin: "10px",
-            backgroundColor: "lightGrey",
+            backgroundColor: "#E8E8E8",
             borderRadius: "2px",
+            width: "30%",
           }}
         >
           <h2>Useful websites for resources:</h2>
           <h3>
             <a href="https://sites.google.com/view/infinityplusone/home?authuser=0">
               Infinity plus one
-            </a>
+            </a>{" "}
             - Fantastic videos going over NCEA exams
           </h3>
           <h3>
-            <a href="https://www.draustinmaths.com/">Dr Austin maths</a>- Great
+            <a href="https://www.draustinmaths.com/">Dr Austin maths</a> - Great
             for printable worksheets for different topics
           </h3>
           <h3>
-            <a href="https://grapher.jake4maths.com/">NZGrapher</a>- Awesome
+            <a href="https://grapher.jake4maths.com/">NZGrapher</a> - Awesome
             Graphing website for NCEA internals
           </h3>
           <h3>
-            <a href="https://nzamtnews.blogspot.com/">NZAMT news</a>- to help
+            <a href="https://nzamtnews.blogspot.com/">NZAMT news</a> - to help
             stay up to date with Mathematics Education
           </h3>
           <h3>
-            <a href="https://nrich.maths.org/">NRich</a>- For rich mathematical
+            <a href="https://nrich.maths.org/">NRich</a> - For rich mathematical
             activities
           </h3>
           <h3>
